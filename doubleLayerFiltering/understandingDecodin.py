19f720fffe62dab32e7b49c8faf8c756b5d502b4
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-08-03 13:59:56 -0500

    added a file to better decode layer and module

---
+++ doubleLayerFiltering/understandingDecodin.py
@@ -0,0 +1,86 @@
+#Imports are meant to be run on snowmass21 server
+from array import array
+from pyLCIO import IOIMPL, EVENT, UTIL
+import ROOT
+from ROOT import TH1D, TH2D, TFile, TLorentzVector, TTree, TMath
+import glob
+from optparse import OptionParser
+
+#Code needed to download data to a root file
+Bfield = 3.56  # T
+parser = OptionParser()
+parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
+                  type=str, default='Output_REC.slcio')
+parser.add_option('-o', '--outFile', help='--outFile ntup_hits_SiTracksNOBIB.root',
+                  type=str, default='ntup_hits_SiTracksNOBIB.root')
+(options, args) = parser.parse_args()
+
+tree = TTree("tracks_tree", "tracks_tree")
+
+fnames = glob.glob("/data/fmeloni/LegacyProductions/before29Jul23/DataMuC_MuColl_v1/muonGun/reco/*100.slcio")
+
+collections=[
+    "AllTracks",
+    "ECALBarrel",
+    "ECALEndcap",
+    "ECalBarrelCollection",
+    "ECalEndcapCollection",
+    "HCALBarrel",
+    "HCALEndcap",
+    "HCALOther",
+    "HCalBarrelCollection",
+    "HCalEndcapCollection",
+    "HCalRingCollection",
+    "IBTrackerHits",
+    "IBTrackerHitsRelations",
+    "IETrackerHits",
+    "IETrackerHitsRelations",
+    "InnerTrackerBarrelCollection",
+    "InnerTrackerEndcapCollection",
+    "JetOut",
+    "LE_LooseSelectedPandoraPFOs",
+    "LE_SelectedPandoraPFOs",
+    "LE_TightSelectedPandoraPFOs",
+    "LooseSelectedPandoraPFOs",
+    "MCParticle",
+    "MCPhysicsParticles",
+    "MUON",
+    "OBTrackerHits",
+    "OBTrackerHitsRelations",
+    "OETrackerHits",
+    "OETrackerHitsRelations",
+    "OuterTrackerBarrelCollection",
+    "OuterTrackerEndcapCollection",
+    "PandoraClusters",
+    "PandoraPFOs",
+    "PandoraStartVertices",
+    "RelationCaloHit",
+    "RelationMuonHit",
+    "SeedTracks",
+    "SelectedPandoraPFOs",
+    "SiTracks",
+    "SiTracks_Refitted",
+    "TightSelectedPandoraPFOs",
+    "VBTrackerHits",
+    "VBTrackerHitsRelations",
+    "VETrackerHits",
+    "VETrackerHitsRelations",
+    "VertexBarrelCollection",
+    "VertexEndcapCollection",
+    "YokeBarrelCollection",
+    "YokeEndcapCollection"
+    ]
+methods=[]
+
+for f in fnames:
+    reader = IOIMPL.LCFactory.getInstance().createLCReader()
+    reader.open(f)
+    events=[event for event in reader]
+    for collection in collections:
+        print(collection + ":")
+        methods.append(dir(events[0].getCollection(collection)))
+        print(methods[-1])
+    print("Parameters:")
+    for i in range(len(collections)):
+        if ("getParameters" in methods[i]):
+            print(collections[i])
