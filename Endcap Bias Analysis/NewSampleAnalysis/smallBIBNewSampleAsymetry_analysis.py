1c38f8dde679bf9d861ae44f02a12b9664aaee5b
Isaac Hirsch <isaac@isaacs-air.lan>
2023-08-18 16:49:48 -0500

    changed files

--- Endcap Bias Analysis/NewSampleAnalysis/smallBIBNewSampleAsymetry_analysis.py
+++ Endcap Bias Analysis/NewSampleAnalysis/smallBIBNewSampleAsymetry_analysis.py
@@ -13,7 +13,7 @@ parser = OptionParser()
 parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
                   type=str, default='Output_REC.slcio')
 parser.add_option('-o', '--outFile', help='--outFile hitsPerLayer',
-                  type=str, default='hitsPerLayer')
+                  type=str, default='noBIBEndcapAnalysis')
 (options, args) = parser.parse_args()
 
 
