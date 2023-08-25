88a8a72f344b761ff3a502fa0dfe01513ed33416
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-08-25 13:50:21 -0500

    fixing range on pseudo rapdity from -2.2,2.2 to -2.4,2.4

--- doubleLayerFiltering/nearestNeighborHash.py
+++ doubleLayerFiltering/nearestNeighborHash.py
@@ -37,7 +37,7 @@ for f in fnames:
     for event in reader:
 
         #Creating bins to sort hits from the second layer into.
-        #The Pseudonapidity spans -2.2 to 2.2 and is split into nPsuedoRap evenly sized bins
+        #The Pseudonapidity spans -2.4 to 2.4 and is split into nPsuedoRap evenly sized bins
         #The Phi spans -pi to pi and is split into nPhi evenly sized bins
         sorting=[]
         for i in range(nPseudoRap):
@@ -64,7 +64,7 @@ for f in fnames:
                 pseudoRapidity=hit.getPositionVec().PseudoRapidity()
                 phi=hit.getPositionVec().Phi()
                 #Hashing the hit into the correct bins
-                sorting[int(((2.2+pseudoRapidity)*nPseudoRap)/4.4)][int(((np.pi+phi)*nPhi)/(2*np.pi))].append((pseudoRapidity,phi))
+                sorting[int(((2.4+pseudoRapidity)*nPseudoRap)/4.8)][int(((np.pi+phi)*nPhi)/(2*np.pi))].append((pseudoRapidity,phi))
 
 
             elif layer==0:
@@ -73,7 +73,7 @@ for f in fnames:
         #Run through every hit in the first layer of the doublet and find its nearest neighbor in terms of delta R on the second layer
         for (pseudoRap,phi) in firstLayerHit:
             #Navive search to be fixed later. Does not work well if the nearest neighbor is not in the same box the hit is in.
-            box=sorting[int(((2.2+pseudoRap)*nPseudoRap)/4.4)][int(((np.pi+phi)*nPhi)/(2*np.pi))]
+            box=sorting[int(((2.4+pseudoRap)*nPseudoRap)/4.8)][int(((np.pi+phi)*nPhi)/(2*np.pi))]
             #Checking if there is anything in the box, if not its skipped and added to nBox which is a measure of how good of an approximation searching 1 box is
             if len(box) !=0:
                 minPseudo=box[0][0]
@@ -117,9 +117,9 @@ for f in fnames:
                 phi=hit.getPositionVec().Phi()
                 #layer/2+4*(side==1) uniquely hashes each outer doublet endcap into a value of 0-7
                 print("Hash:" + str(int(layer/2+4*(side==1))))
-                print("Pseudo:" + str(int(((2.2+pseudoRapidity)*nPseudoRap)/4.4)))
+                print("Pseudo:" + str(int(((2.4+pseudoRapidity)*nPseudoRap)/4.8)))
                 print("Phi:" + str(int(((np.pi+phi)*nPhi)/(2*np.pi))))
-                sorting[int(layer/2+4*(side==1))][int(((2.2+pseudoRapidity)*nPseudoRap)/4.4)][int(((np.pi+phi)*nPhi)/(2*np.pi))].append((pseudoRapidity,phi))
+                sorting[int(layer/2+4*(side==1))][int(((2.4+pseudoRapidity)*nPseudoRap)/4.8)][int(((np.pi+phi)*nPhi)/(2*np.pi))].append((pseudoRapidity,phi))
 
             #All other hits are in the first layer of a doublet
             else:
@@ -129,7 +129,7 @@ for f in fnames:
             for (psuedoRap,phi, pixel) in firstLayerHit:
                 #Navive search to be fixed later
                 #Pixel represents the endcap hash we should be looking at
-                box=sorting[pixel][int(((2.2+pseudoRapidity)*nPseudoRap)/4.4)][int(((np.pi+phi)*nPhi)/(2*np.pi))]
+                box=sorting[pixel][int(((2.4+pseudoRapidity)*nPseudoRap)/4.8)][int(((np.pi+phi)*nPhi)/(2*np.pi))]
                 if len(box) !=0:
                     minPseudo=box[0][0]
                     minPhi=box[0][1]
