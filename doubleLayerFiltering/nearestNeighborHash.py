92fad57c92c51f85613e5b0929384c675a38009a
Isaac Hirsch <isaac@isaacs-air.lan>
2023-08-26 15:56:40 -0500

    added file to analyze hit level doublet data, as well as updated the hasing file to calculate delta phi, psuedo, and R instead of just the absolute value of them it was calculating earlier

--- doubleLayerFiltering/nearestNeighborHash.py
+++ doubleLayerFiltering/nearestNeighborHash.py
@@ -7,16 +7,22 @@ from pyLCIO import IOIMPL, EVENT, UTIL
 parser = OptionParser()
 parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
                   type=str, default='Output_REC.slcio')
-parser.add_option('-o', '--outFile', help='--outFile nearestPair250NoBIB',
-                  type=str, default='nearestPair250NoBIB')
+parser.add_option('-o', '--outFile', help='--outFile nearestPairBIB',
+                  type=str, default='nearestPairBIB')
 (options, args) = parser.parse_args()
 
-#Gather all the files you want to run over
-fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_250_1000/muonGun_pT_250_1000_reco_*.slcio")
+#Gather all the files you want to run over.
+#Comment out all but 1 fnames
+#BIB
+fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/recoBIB/photonGun_pT_0_50/photonGun_pT_0_50_reco_2[123]00.slcio")
+#0-50 pt muons
+#fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/muonGun_pT_0_50_reco_*.slcio")
+#250-1000 pt muons
+#fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_250_1000/muonGun_pT_250_1000_reco_*.slcio")
 
 #Setting number of bins for the sorting function
-nPseudoRap=200
-nPhi=200
+nPseudoRap=50
+nPhi=50
 #Creating output data storage
 deltaPseudo=[]
 deltaPhi=[]
@@ -76,15 +82,17 @@ for f in fnames:
             box=sorting[int(((2.4+pseudoRap)*nPseudoRap)/4.8)][int(((np.pi+phi)*nPhi)/(2*np.pi))]
             #Checking if there is anything in the box, if not its skipped and added to nBox which is a measure of how good of an approximation searching 1 box is
             if len(box) !=0:
-                minPseudo=box[0][0]
-                minPhi=box[0][1]
+                minPseudo=np.abs(box[0][0]-pseudoRap)
+                minPhi=np.abs(box[0][1]-phi)
                 minRad=np.sqrt(minPseudo**2+minPhi**2)
                 for (boxPseduo, boxPhi) in box[1:]:
-                    boxRad=np.sqrt(boxPseduo**2+boxPhi**2)
-                    if minRad > boxRad:
-                        minRad=boxRad
-                        minPhi=boxPhi
-                        minPseudo=boxPseduo
+                    deltaPseudoPos=np.abs(boxPseduo-pseudoRap)
+                    deltaPhiPos=np.abs(boxPhi-phi)
+                    deltaRadPos=np.sqrt(deltaPseudoPos**2+deltaPhiPos**2)
+                    if minRad > deltaRadPos:
+                        minRad=deltaRadPos
+                        minPhi=deltaPhiPos
+                        minPseudo=deltaPseudoPos
                 deltaPseudo[0].append(minPseudo)
                 deltaPhi[0].append(minPhi)
                 deltaR[0].append(minRad)
@@ -129,15 +137,17 @@ for f in fnames:
             #Pixel represents the endcap hash we should be looking at
             box=sorting[pixel][int(((2.4+pseudoRapidity)*nPseudoRap)/4.8)][int(((np.pi+phi)*nPhi)/(2*np.pi))]
             if len(box) !=0:
-                minPseudo=box[0][0]
-                minPhi=box[0][1]
+                minPseudo=np.abs(box[0][0]-pseudoRap)
+                minPhi=np.abs(box[0][1]-phi)
                 minRad=np.sqrt(minPseudo**2+minPhi**2)
                 for (boxPseduo, boxPhi) in box[1:]:
-                    boxRad=np.sqrt(boxPseduo**2+boxPhi**2)
-                    if minRad > boxRad:
-                        minRad=boxRad
-                        minPhi=boxPhi
-                        minPseudo=boxPseduo
+                    deltaPseudoPos=np.abs(boxPseduo-pseudoRap)
+                    deltaPhiPos=np.abs(boxPhi-phi)
+                    deltaRadPos=np.sqrt(deltaPseudoPos**2+deltaPhiPos**2)
+                    if minRad > deltaRadPos:
+                        minRad=deltaRadPos
+                        minPhi=deltaPhiPos
+                        minPseudo=deltaPseudoPos
                 #Add one to hash in delta{} because they need to account for the barrel doublet in the first spot
                 deltaPseudo[1+pixel].append(minPseudo)
                 deltaPhi[1+pixel].append(minPhi)
