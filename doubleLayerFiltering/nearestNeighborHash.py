d936464c7960831e6885eed1cc46c9b87e08eee0
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-08-28 16:53:18 -0500

    creating depth-first search object that is still in progress

--- doubleLayerFiltering/nearestNeighborHash.py
+++ doubleLayerFiltering/nearestNeighborHash.py
@@ -21,8 +21,92 @@ fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/recoBIB/photonGun_pT_0_50
 #fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_250_1000/muonGun_pT_250_1000_reco_*.slcio")
 
 #Setting number of bins for the sorting function
-nPseudoRap=50
-nPhi=50
+#Pseudorapditity spans -2.4 to 2.4
+nPseudoRap=200
+#Phi spans -pi to pi
+nPhi=200
+
+class depthFirstSearch(object):
+    #Object for depth-first search to find the nearest hit in boxes to a point.
+    def __init__(self, boxes, points):
+        assert type(boxes)==list, "box is wrong type"
+        assert type(points)==list, "point is wrong type"
+        self.boxes=boxes
+        #Boxes is a list of all hits on the second layer of a doublet layer split into small boxes in phi and pseudorapidity
+        self.points=points
+        #the point on the first layer of the doublet we are trying to find the nearest hit to
+
+    def search(self):
+        for pseudo, phi in self.points:
+            #Starting a priority queue for depth first search and intializing the current box the hit is in as the first seach
+            #All of the queues are synced so that indexing the nth index of all of them returns info on the same box
+            pseudoIndex=int(((2.4+pseudo)*nPseudoRap)/4.8)
+            phiIndex=int(((np.pi+phi)*nPhi)/(2*np.pi))
+            boxQueue=[self.boxes[pseudoIndex][phiIndex]]
+            RQueue=[0]
+            #Keep track of the steps to get to each box from the point
+            #Specifically if phi steps=0 then the closest point is on the wall of the box with the same phi value
+            #If phi steps<0, then the closest point is on the wall with the maximum phi value
+            #If phi steps>0, then the closest point is on the wall with the minimum phi value
+            #The above statements are also true for pseudorapidity
+            phiStepsQueue=[0]
+            pseudoStepsQue=[0]
+            #repeatDict is a dictionary of all the steps that will remember if we haev already searched a box
+            #keys will be tuples (phiSteps, pseudoSteps)
+            repeatDict={(0,0)}
+
+            #Searching through priority queue until we can guarentee we have found that closest point
+            i=0
+            minR=float('inf')
+            minPhi=float('inf')
+            minPseudo=float('inf')
+            #While loop will stop as soon as the box with the closest possible point is farther then the closests point we already found
+            while RQueue[i] < minR:
+                boxResult=self.searchBox(boxQueue[i])
+                if boxResult:
+                    if boxResult[0] < minR:
+                        minR=boxResult[0]
+                        minPseudo=boxResult[1]
+                        minPhi=boxResult[2]
+                for phiStep in (-1,1):
+                    totPhiSteps=phiStepsQueue[i]+phiStep
+                    for pseudoStep in (-1,1):
+                        totPseudoSteps=pseudoStepsQue[i]+pseudoStep
+                        if not ((totPhiSteps,totPseudoSteps) in repeatDict):
+                            deltaMinPhi=0
+                            deltaMinR=0
+                            if totPseudoSteps==0:
+                                deltaMinPseudo=0
+                            elif totPseudoSteps<0:
+                                deltaMinPseudo=((2.4+pseudo)*nPseudoRap/4.8-pseudoIndex)/nPseudoRap+(-totPseudoSteps+1)*4.8/nPseudoRap
+                            else:
+                                deltaMinPseudo=-((2.4+pseudo)*nPseudoRap/4.8-pseudoIndex)/nPseudoRap+totPseudoSteps*4.8/nPseudoRap
+                            if totPhiSteps==0:
+                                deltaMinPhi=0
+                            elif totPhiSteps<0:
+                                deltaMinPhi=((np.pi+phi)*nPhi/(2*np.pi)-phiIndex)/nPhi+(-totPhiSteps+1)*2*np.pi/nPhi
+                            else:
+                                deltaMinPhi=-((np.pi+phi)*nPhi/(2*np.pi)-phiIndex)/nPhi+totPhiSteps*2*np.pi/nPhi
+                    
+
+
+    def searchBox(self, box, pseudo,phi):
+        if len(box):
+            minPseudo=np.abs(box[0][0]-pseudo)
+            minPhi=np.abs(box[0][1]-phi)
+            minR=np.sqrt(minPseudo**2+minPhi**2)
+            for boxPseudo, boxPhi in box[1:]:
+                deltaPseudo=np.abs(boxPseudo-pseudo)
+                deltaPhi=np.abs(boxPhi-phi)
+                deltaR=np.sqrt(deltaPhi**2+deltaPseudo**2)
+                if deltaR < minR:
+                    minR=deltaR
+                    minPhi=deltaPhi
+                    minPseudo=deltaPseudo
+            return (minR,minPseudo,minPhi)
+        else:
+            return False
+
 #Creating output data storage
 deltaPseudo=[]
 deltaPhi=[]
@@ -57,7 +141,7 @@ for f in fnames:
         encoding=hitsCollection.getParameters().getStringVal(EVENT.LCIO.CellIDEncoding)
         decoder=UTIL.BitField64(encoding)
 
-        firstLayerHit=[]
+        firstLayerHit=[[],[]]
 
         for hit in hitsCollection:
             #Decoder
@@ -74,7 +158,8 @@ for f in fnames:
 
 
             elif layer==0:
-                firstLayerHit.append((hit.getPositionVec().PseudoRapidity(),hit.getPositionVec().Phi()))
+                firstLayerHit[0].append(hit.getPositionVec().PseudoRapidity())
+                firstLayerHit[1].append(hit.getPositionVec().Phi())
         
         #Run through every hit in the first layer of the doublet and find its nearest neighbor in terms of delta R on the second layer
         for (pseudoRap,phi) in firstLayerHit:
