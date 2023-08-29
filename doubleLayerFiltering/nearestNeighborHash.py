776346da70cd75fcae51ed17c9df719cbe291c50
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-08-29 13:13:59 -0500

    added more assertations and revised a few mistakes

--- doubleLayerFiltering/nearestNeighborHash.py
+++ doubleLayerFiltering/nearestNeighborHash.py
@@ -28,20 +28,30 @@ nPhi=200
 
 class depthFirstSearch(object):
     #Object for depth-first search to find the nearest hit in boxes to a point.
-    def __init__(self, boxes, points):
-        assert type(boxes)==list, "box is wrong type"
-        assert type(points)==list, "point is wrong type"
+    def __init__(self, boxes : list, points : list):
+        #Boxes should be a list of lists of hits on the second doublet layer where the first list represents the pseudo hash and the second represents the phi hash
+        #Points should be a list of 2 length tuples that represent all hits on the first doublet layer. The first element of the tuple should be pseudorapidity and the second should be phi
+        #Asserting boxes and points are the correct format
+        assert type(boxes)==list, "box is wrong type. It is " + str(type(boxes))
+        for box in boxes:
+            assert type(box)==list, "an element of box is the wrong type. It is " + str(type(box))
+        assert type(points)==list, "points is wrong type. It is " + str(type(points))
+        for point in points:
+            assert type(point)==tuple, "an element of points is the wrong type. It is " + str(type(point))
+            assert len(point)==2, "an element of points contains too many elements. It has " + str(len(point)) + " elements."
         self.boxes=boxes
-        #Boxes is a list of all hits on the second layer of a doublet layer split into small boxes in phi and pseudorapidity
         self.points=points
-        #the point on the first layer of the doublet we are trying to find the nearest hit to
 
-    def search(self):
+    def search(self)-> list:
+        #Searches for the nearest hit in box for all points, returns a list of tuples (delta R,delta prseudorapditity, delta phi)
         output=[]
         #Loop over all hits on the first doublet layer to find each of their closest points
         for pseudo, phi in self.points:
+            assert type(pseudo)==float, "the first element in a point is the wrong type. It is a " + str(type(pseudo))
+            assert type(phi)==float, "the second element in a point is the wrong type. It is a " + str(type(phi))
             #Starting a priority queue for depth first search and intializing the current box the hit is in as the first seach
             #All of the queues are synced so that indexing the nth index of all of them returns info on the same box
+            #If this version is too slow or takes up too much memory, you can change it such that the search removes items from the queue after it looks at them
             pseudoIndex=int(((2.4+pseudo)*nPseudoRap)/4.8)
             phiIndex=int(((np.pi+phi)*nPhi)/(2*np.pi))
             boxQueue=[self.boxes[pseudoIndex][phiIndex]]
@@ -53,8 +63,8 @@ class depthFirstSearch(object):
             #The above statements are also true for pseudorapidity
             phiStepsQueue=[0]
             pseudoStepsQue=[0]
-            #repeatDict is a dictionary of all the steps that will remember if we haev already searched a box
-            #keys will be tuples (phiSteps, pseudoSteps)
+            #repeatDict is a dictionary of all the steps already searched and will be used to remember if we have already searched a box
+            #keys will be tuples (pseudoSteps, phiSteps)
             repeatDict={(0,0)}
             #Searching through priority queue until we can guarentee we have found that closest point
             i=0
@@ -79,41 +89,50 @@ class depthFirstSearch(object):
                     #require the next box to be one we have not seen before and to within out psuedo and phi bounds
                     if (not ((totPhiSteps,totPseudoSteps) in repeatDict)) & (np.abs(pseudo+totPseudoSteps*4.4/nPseudoRap) < 2.4) & (np.abs(phi + totPhiSteps*2*np.pi/nPhi) <= np.pi):
                         #Add the box to the list of boxes we have already seen
-                        repeatDict[(totPhiSteps,totPseudoSteps)]=True
+                        repeatDict[(totPseudoSteps, totPhiSteps)]=True
                         #Calculate the nearest possible R
                         if totPseudoSteps==0:
                             deltaMinPseudo=0
                         elif totPseudoSteps<0:
-                            deltaMinPseudo= (2.4+pseudo)%(4.8*nPseudoRap)+(-totPseudoSteps+1)*4.8/nPseudoRap
+                            deltaMinPseudo= -(2.4+pseudo)%(4.8*nPseudoRap)+(-totPseudoSteps+1)*4.8/nPseudoRap
                         else:
                             deltaMinPseudo= -(2.4+pseudo)%(4.8*nPseudoRap)+totPseudoSteps*4.8/nPseudoRap
                         if totPhiSteps==0:
                             deltaMinPhi=0
                         elif totPhiSteps<0:
-                            deltaMinPhi=((np.pi+phi)%(2*np.pi*nPhi))+(-totPhiSteps+1)*2*np.pi/nPhi
+                            deltaMinPhi= -((np.pi+phi)%(2*np.pi*nPhi))+(-totPhiSteps+1)*2*np.pi/nPhi
                         else:
                             deltaMinPhi= -((np.pi+phi)%(2*np.pi*nPhi))+totPhiSteps*2*np.pi/nPhi
                         deltaMinR=np.sqrt(deltaMinPhi**2+deltaMinPseudo**2)
                         #check whether the box might contain the closest point
                         if deltaMinR < minR:
                             #Find where to insert it in sorted RQueue array and then assert all the other values in the same location in the other arrays
-                            for j in range(len(RQueue)-i):
+                            RLen=len(RQueue)
+                            for j in range(RLen-i):
                                 if RQueue[-j] < deltaMinR:
-                                    RQueue.insert(-j, deltaMinR)
-                                    boxQueue.insert(-j,self.boxes[pseudoIndex + totPseudoSteps][phiIndex+totPhiSteps])
-                                    phiStepsQueue.insert(-j,totPhiSteps)
-                                    pseudoStepsQue.insert(-j,totPseudoSteps)
+                                    #Must of RLen-j instead of just -j, because -j inserts the item before the index while RLen-j inserts it after the index
+                                    RQueue.insert(RLen-j, deltaMinR)
+                                    boxQueue.insert(RLen-j,self.boxes[pseudoIndex + totPseudoSteps][phiIndex+totPhiSteps])
+                                    phiStepsQueue.insert(RLen-j,totPhiSteps)
+                                    pseudoStepsQue.insert(RLen-j,totPseudoSteps)
                                     break
                 i+=1
-            output.append(minR, minPseudo, minPhi)  
+            output.append((minR, minPseudo, minPhi))  
         return output      
 
-    def searchBox(self, box, pseudo,phi):
+    def searchBox(self, box: list, pseudo: float,phi: float):
+        #Searches a box for the nearest hit in delta R to (pseudo, phi)
+        #If the box is empty it returns false, otherwise it returns (minR,minPseudo,minPhi) of the closest hit
         if len(box):
+            assert len(box[0])==2, "a box has a hit of the wrong size. It is size " + str(len(box[0]))
+            assert type(box[0][0])==float, "the first element of a hit in a box is the wrong type. It is a " + str(type(box[0][0]))
+            assert type(box[0][1])==float, "the second element of a hit in a box is the wrong type. It is a " + str(type(box[0][1]))
             minPseudo=np.abs(box[0][0]-pseudo)
             minPhi=np.abs(box[0][1]-phi)
             minR=np.sqrt(minPseudo**2+minPhi**2)
             for boxPseudo, boxPhi in box[1:]:
+                assert type(boxPseudo)==float, "the first element of a hit in a box is the wrong type. It is a " + str(type(box[0][0]))
+                assert type(boxPhi)==float, "the second element of a hit in a box is the wrong type. It is a " + str(type(box[0][1]))
                 deltaPseudo=np.abs(boxPseudo-pseudo)
                 deltaPhi=np.abs(boxPhi-phi)
                 deltaR=np.sqrt(deltaPhi**2+deltaPseudo**2)
