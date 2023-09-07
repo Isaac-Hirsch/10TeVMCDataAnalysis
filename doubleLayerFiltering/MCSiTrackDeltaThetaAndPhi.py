3d5f880c108b8b3f4dd3e488f23bace4615fc517
Isaac Hirsch <isaac@isaacs-air.lan>
2023-09-06 20:04:49 -0500

    McTracks file

---
+++ doubleLayerFiltering/MCSiTrackDeltaThetaAndPhi.py
@@ -0,0 +1,26 @@
+import numpy as np
+import glob
+from optparse import OptionParser
+import json
+from pyLCIO import IOIMPL, EVENT, UTIL
+
+parser = OptionParser()
+parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
+                  type=str, default='Output_REC.slcio')
+parser.add_option('-o', '--outFile', help='--outFile nearestPairBIB',
+                  type=str, default='nearestPairBIB')
+(options, args) = parser.parse_args()
+
+fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/muonGun_pT_0_50_reco_1000.slcio")
+
+#Loop over every file
+for f in fnames:
+    reader = IOIMPL.LCFactory.getInstance().createLCReader()
+    reader.open(f)
+
+    #Loop over events in the file
+    for event in reader:
+        tracksCollection = event.getCollection("MCParticle_SiTracks_Refitted")
+        print(dir(tracksCollection))
+        for track in tracksCollection:
+            print(dir(track))
