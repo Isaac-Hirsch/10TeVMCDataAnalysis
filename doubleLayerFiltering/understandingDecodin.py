e48d298f7088ba7d0db97f6d8552561eb963a15d
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-09-25 14:54:58 -0500

    changed to Z

--- doubleLayerFiltering/understandingDecodin.py
+++ doubleLayerFiltering/understandingDecodin.py
@@ -101,5 +101,5 @@ for f in fnames:
             print(f"Gen: {gen}")
             print(f"Decay: {decay}")
             print(f"Overlay: {overlay}")
-            print(f"Vertex Z: {vertex.GetXYZ()}")
+            print(f"Vertex Z: {vertex.Z()}")
             print("")
